c0232fdc99b59d7852d4dfe06f40f7d2b885c3db
navjot0 <rohitofficial2001@gmail.com>
2023-01-23 11:15:11 +0530

    implemented saving in browser local storage

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,7 +5,6 @@ import BookDetails from './BookDetails';
 
 const BookList = () => {
 const {books} = useContext(BookContext);
-console.log(books)
   return books.length ? (
     <div className="book-list">
         <ul>
--- src/contetxs/BookContext.js
+++ src/contetxs/BookContext.js
@@ -1,10 +1,19 @@
-import React, {createContext, useReducer} from 'react'
+import React, {createContext, useReducer, useEffect} from 'react'
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-const [books, dispatch] = useReducer(bookReducer , []);
+const [books, dispatch] = useReducer(bookReducer , [], ()=>{
+  // Third Argument returns a value that is Assigned as initial value for the state and the second argument i.e [], is ignored 
+  const localData = localStorage.getItem('books');
+  return localData ? JSON.parse(localData) : [];
+});
+
+//  Saving Data To Browser Local Storage 
+useEffect(() => {
+  localStorage.setItem('books', JSON.stringify(books))
+}, [books]);
 
   return (
     <BookContext.Provider value={{books, dispatch}}>
